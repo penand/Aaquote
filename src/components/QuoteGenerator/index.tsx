cbb7dd7d5bc89f6dd118587c5bf9a07ecb9ad024
penand <marzone.ap@gmail.com>
2022-01-31 18:42:13 +0100

    feat(quote-generator): show random quote

--- src/components/QuoteGenerator/index.tsx
+++ src/components/QuoteGenerator/index.tsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react";
+import { getAll } from "../../services/data-service";
+import { QuoteType } from "../../typings";
+
 const QuoteGenerator = () => {
+    const [quotes, setQuotes] = useState<QuoteType[]>([]);
+
+    useEffect(() => {
+        getAll().then((response) => {
+            console.log('response:', response.data);
+            setQuotes(response.data);
+        }).catch((err) => {
+            console.log('ERROR: ', err);
+        })
+    }, []);
+
+
+    const randomIndex = Math.floor(Math.random() * quotes?.length);
+
     return (
-        <p>Quote generator goes here</p>
+        <div>
+            {
+                quotes.length > 0 ?
+                    <h1 color="white">
+                        {quotes[randomIndex].title}
+                    </h1>
+                : null
+            }
+        </div>
     );
 }
 
